cf34a72d6a857e02e15acd0ee8acc7126e017127
Megan Dotto <megan.dotto@gmail.com>
2018-03-22 06:18:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  puts "#{number} SECOND(S)!"
+  number -=1
+end
+  "HAPPY NEW YEAR"
+end
+
+def countdown_with_sleep(number)
+  sleep number
 end
